5e562d3a2aede92c9a2a28073222481e1b563440
Vijayaragavan Nagayasamy <vnagayasamy@ViaSat-Ubuntu-18-v20210501.nodomain>
2022-08-15 13:39:20 +0000

    Added functions for following operations.  - printing list in reverse order without actually reversing  - swap two nodes based on two values  - swap two nodes with the distance of K  - reverse sublist for the given number of nodes  - reverse the whole linkedlist

--- C++/linkedlist.cpp
+++ C++/linkedlist.cpp
@@ -4,15 +4,42 @@ using namespace std;
 /*
 OUTPUT:
 -------
+Inserting node for 5
+Inserting node for 4
+Inserting node for 3
+Inserting node for 2
+Inserting node for 1
 --- LinkedList ---
 5->4->3->2->1->NULL
 --- Deleting 3 ---
 --- LinkedList ---
 5->4->2->1->NULL
---- Searching 1 ---
-Is available ?  1
---- Searching 3 ---
-Is available ?  0
+Is available ?  --- Searching 1 ---
+1
+Is available ?  --- Searching 3 ---
+0
+--- Printing Linked list in reverse order ---
+1->2->4->5->NULL
+--- Sequence of Linkedlist ---
+5->4->2->1->NULL
+Swapping the nodes that holds 4 and 2
+5->2->4->1->NULL
+Swapping the nodes that holds 2 and 1
+5->1->4->2->NULL
+Swapping the nodes that holds 5 and 4
+4->1->5->2->NULL
+Inserting node for 3
+Inserting node for 10
+Inserting node for 11
+--- LinkedList ---
+4->1->5->2->3->10->11->NULL
+--- Reversing LinkedList by 3
+5->1->4->10->3->2->11->NULL
+--- Swapping the nodes with the distance of 3
+4->1->5->2->3->10->11->NULL
+--- Reversing LinkedList ---
+11->10->3->2->5->1->4->NULL
+
 */
 class linkedList {
 
@@ -33,6 +60,7 @@ class linkedList {
     //insert at the back
     struct llNode* insertllNode(struct llNode* &head, struct llNode* &tail, struct llNode* node) 
     {
+        cout << "Inserting node for " << node->data << endl;
         if(head == NULL)
         {
             head = tail = node;
@@ -107,11 +135,159 @@ class linkedList {
             cout << "NULL" << endl;
         }
     }
+    
+    //Print the linkedlist nodes in reverse without reversing the pointers.
+    //Ref: https://www.geeksforgeeks.org/print-reverse-of-a-linked-list-without-actually-reversing/
+    void printReverse(struct llNode* head)
+    {
+        if(head != NULL)
+        {
+            printReverse(head->next);
+            cout << head->data << "->" ;
+        }
+    }
+
+void swapllNodesByPointers(struct llNode* &head, int d1, int d2)
+    {
+        struct llNode *p1, *c1;
+        struct llNode *p2, *c2,*tmp;
+        
+        p1 = p2 = head;
+        c1 = c2 = (head->data == d1 ) ? head : head->next;
+        
+        cout << "Swapping the nodes that holds " << d1 << " and " << d2 << endl;
+        
+        while((c1 != NULL) && (c2 != NULL))
+        {
+            
+            if( c1->data == d1 && c2->data == d2)
+            {
+                
+                 /* Swapping data with head node */
+                  if(head == c1)
+                    head = c2 ;              
+                /* swap */
+                tmp = c2->next;
+                c2->next = c1->next;
+                p1->next = c2;
+                
+                
+                if(c1 == p2) /* swapping adjacent nodes */
+                    p2 = c2;
+                p2->next = c1;
+                c1->next = tmp;
+                /* updating the tail pointer if there is any change in tail node */
+                tail = (tail == c1) ? c2 : ((tail == c2) ? c1 : tail ) ;
+                
+                break;
+            }
+            else if( c1->data == d1)
+            {
+                /* Move p2, c2 until it reaches d2 */
+                p2 = c2;
+                c2 = c2->next;
+            }
+            else if( c2->data == d2)
+            {
+                /* Move p1, c1 until it reaches d1 */
+                p1 = c1;
+                c1 = c1->next;
+            }
+            else
+            {
+                p1 = p2 = c1;
+                c1 = c2 = c1->next;
+            }
+        }
+    }
+    
+    struct llNode* swapllNodesByK(struct llNode* head, int k)
+    {
+        struct llNode *prev, *curr,*tmp;
+        int cnt = 1;
+        
+        if(head == NULL)
+            return NULL;
+        
+        prev = head;
+        curr = head->next;
+        
+        while((curr != NULL) && (cnt < (k-1)))
+        {
+            
+            prev = curr;
+            curr = curr->next;
+            cnt++;
+        }
+       
+        if(curr != NULL)
+        {
+            //cout << "Swapping nodes " << head->data << " " << curr->data;
+            tmp = curr->next;
+            prev->next = head;
+            curr->next = head->next;
+            head->next = tmp;
+            
+            head->next = swapllNodesByK(head->next, k);
+            
+            return(curr);
+        }
+        else
+        {
+            return(head);
+        }
+        
+    }
+    
+    struct llNode* reverseLinkedListByK(struct llNode* head, int k )
+    {
+        struct llNode *prev,*curr,*tmp;
+        
+        int cnt = 1;
+        prev = head;
+        curr = head->next;
+        while((curr != NULL) && (cnt < k))
+        {
+            //cout << prev->data << " " << curr->data << endl;
+            tmp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = tmp;
+            cnt++;
+        }
+
+        if(curr != NULL)
+            head->next = reverseLinkedListByK(curr,k);
+        
+        return(prev);
+    }
+
+    struct llNode* reverseLinkedList(struct llNode *head)
+    {
+        struct llNode *tmp = NULL;
+        struct llNode *prev,*curr;
+        struct llNode *tail ;
+        
+        prev = tail = head;
+        curr = head->next;
+        while(curr != NULL)
+        {
+            tmp = curr->next;
+            curr->next = prev;
+            
+            prev = curr;
+            curr = tmp;
+        }
+        tail->next = NULL;
+        return(prev);
+    }
 };
 
 int main()
 {
     linkedList sll;
+
+    int K = 3;
     
     sll.insertllNode(sll.head,sll.tail,sll.createllNode(5));
     sll.insertllNode(sll.head,sll.tail,sll.createllNode(4));
@@ -128,6 +304,35 @@ int main()
     cout << "Is available ?  " << sll.searchllNode(sll.head,1) << endl;
     cout << "Is available ?  " << sll.searchllNode(sll.head,3) << endl;
     
+    cout << "--- Printing Linked list in reverse order ---" << endl;
+    sll.printReverse(sll.head);
+    cout << "NULL" << endl;    
+
+    cout << "--- Sequence of Linkedlist ---" << endl;
+    sll.traversellNode(sll.head);
+    sll.swapllNodesByPointers(sll.head,4,2);
+    sll.traversellNode(sll.head);
+    sll.swapllNodesByPointers(sll.head,2,1);
+    sll.traversellNode(sll.head);  
+     sll.swapllNodesByPointers(sll.head,5,4);
+    sll.traversellNode(sll.head); 
+
+    sll.insertllNode(sll.head,sll.tail,sll.createllNode(3));
+    sll.insertllNode(sll.head,sll.tail,sll.createllNode(10));
+    sll.insertllNode(sll.head,sll.tail,sll.createllNode(11));
+
+    cout << "--- LinkedList ---" << endl;
+    sll.traversellNode(sll.head);
+    cout << "--- Reversing LinkedList by " << K << endl; 
+    sll.head = sll.reverseLinkedListByK(sll.head,K);
+    sll.traversellNode(sll.head);
     
-    
+    cout << "--- Swapping the nodes with the distance of " << K << endl;
+    sll.head = sll.swapllNodesByK(sll.head,3);
+    sll.traversellNode(sll.head);    
+  
+    cout << "--- Reversing LinkedList ---" << endl;
+    sll.head = sll.reverseLinkedList(sll.head);
+    sll.traversellNode(sll.head);
+ 
 }
